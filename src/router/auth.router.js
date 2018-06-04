faa2914c61e43d1b7217a78dd17dbac8a284e428
David <davidlee93@gmail.com>
2018-06-04 10:06:25 -0700

    console log

--- src/router/auth.router.js
+++ src/router/auth.router.js
@@ -1,36 +1,34 @@
-'use strict';
-const express = require('express');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const jwt = require('jsonwebtoken');
+"use strict";
+const express = require("express");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const jwt = require("jsonwebtoken");
 
-const config = require('../config/config');
+const config = require("../config/config");
 const authRouter = express.Router();
 
 const createAuthToken = function(user) {
-  return jwt.sign({user}, config.JWT_SECRET, {
+  return jwt.sign({ user }, config.JWT_SECRET, {
     subject: user.email,
     expiresIn: config.JWT_EXPIRY,
-    algorithm: 'HS256'
+    algorithm: "HS256"
   });
 };
 
-
-const localAuth = passport.authenticate('local', {session: false});
+const localAuth = passport.authenticate("local", { session: false });
 authRouter.use(bodyParser.json());
 // The user provides a username and password to login
-authRouter.post('/login', localAuth, (req, res) => {
-  console.log(req)
+authRouter.post("/login", localAuth, (req, res) => {
   const authToken = createAuthToken(req.user.serialize());
-  res.json({authToken});
+  res.json({ authToken });
 });
 
-const jwtAuth = passport.authenticate('jwt', {session: false});
+const jwtAuth = passport.authenticate("jwt", { session: false });
 
 // The user exchanges a valid JWT for a new one with a later expiration
-authRouter.post('/refresh', jwtAuth, (req, res) => {
+authRouter.post("/refresh", jwtAuth, (req, res) => {
   const authToken = createAuthToken(req.user);
-  res.json({authToken});
+  res.json({ authToken });
 });
 
-module.exports = {authRouter};
+module.exports = { authRouter };
